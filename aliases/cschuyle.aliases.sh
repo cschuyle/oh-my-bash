a772b52e5543a2e12e6089c673ff681bcae0e3d9
Carlton Schuyler <carl@dragnon.com>
2020-07-20 15:53:34 -0600

    'dirt' alias for make temp dir

--- aliases/cschuyle.aliases.sh
+++ aliases/cschuyle.aliases.sh
@@ -2,3 +2,4 @@ alias k=kubectl
 alias gs='git status'
 alias ll='ls -ltrh'
 alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew" # from https://github.com/pyenv/pyenv/issues/106
+alias dirt='pushd "$(mktemp -d)"'
