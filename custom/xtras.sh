3581784e0b0c8efcf7cd050586017b9aa597d7f5
Carlton Schuyler <cschuyler@vmware.com>
2020-10-05 15:03:21 -0600

    Add direnv

--- custom/xtras.sh
+++ custom/xtras.sh
@@ -30,3 +30,5 @@ eval "$(rbenv init -)"
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
 eval `/Applications/DjView.app/Contents/setpath.sh`
+
+eval "$(direnv hook bash)"
